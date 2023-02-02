b9771b35d803a643cb409a5c2372a69dc16671b0
David <121988326+Davsau@users.noreply.github.com>
2023-02-01 21:44:36 -0800

    Two JS files using modules

---
+++ 005/script.js
@@ -0,0 +1,14 @@
+/**
+ * here
+ * another comment
+ * with like a block thing
+ * usually at the top of this page
+ */
+
+
+/*
+more comments 
+*/
+
+// here is a comment in js
+alert("illustrating comments");
